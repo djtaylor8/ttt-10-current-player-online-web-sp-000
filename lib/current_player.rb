e60bf557c0c4160ea29361cc3b33b986e8b3ee83
DJ Taylor <dtaylor88@gmail.com>
2021-05-28 21:06:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board) % 2) == 0 
-    puts "X"
+  if turn_count(board) % 2 == 0 
+    "X"
   else 
-    puts "O"
+    "O"
   end
 end
